4ccb638c099e9046ed93543d5520b86b43305907
Celso Inacio <celso@enssure.com.br>
2023-09-07 22:00:31 -0300

    add: matcher test

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -23,14 +19,5 @@ var scanCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(scanCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	scanCmd.Flags().BoolP("dir", "d", false, "Directory to scan")
-	scanCmd.Flags().BoolP("verbose", "v", false, "Enable verbose log")
 }
---
+++ services/matcher_test.go
@@ -0,0 +1,30 @@
+package services
+
+import "testing"
+
+func TestGetFileInfo(t *testing.T) {
+	fileInfo := GetFileInfo("1988_example_video_S01E01_720p_h.264.mp4", "/path/to/video/")
+	if fileInfo.Year != "1988" {
+		t.Errorf("Expected empty year, got '%s'", fileInfo.Year)
+	}
+
+	if fileInfo.Season != "01" {
+		t.Errorf("Expected season '01', got '%s'", fileInfo.Season)
+	}
+
+	if fileInfo.Episode != "01" {
+		t.Errorf("Expected episode '01', got '%s'", fileInfo.Episode)
+	}
+
+	if fileInfo.SanitizedName != "1988 example video S01E01 720p h 264" {
+		t.Errorf("Expected sanitized name 'example video S01E01 720p', got '%s'", fileInfo.SanitizedName)
+	}
+
+	if fileInfo.Resolution != "720p" {
+		t.Errorf("Expected resolution '720p', got '%s'", fileInfo.Resolution)
+	}
+
+	if fileInfo.Codec != "h 264" {
+		t.Errorf("Expected empty codec, got '%s'", fileInfo.Codec)
+	}
+}
