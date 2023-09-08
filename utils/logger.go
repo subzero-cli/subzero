efa91e6fb1d9da94c07893be79f7e67122e43ab9
Celso Inacio <celso@enssure.com.br>
2023-09-07 21:27:26 -0300

    add: managed logger to utils package

---
+++ utils/logger.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type Logger struct {
+	Verbose bool
+}
+
+var logger *Logger
+
+func NewLogger(verbose bool) *Logger {
+	logger = &Logger{
+		Verbose: verbose,
+	}
+	return logger
+}
+
+func GetLogger() *Logger {
+	return logger
+}
+
+func (l *Logger) Info(message string) {
+	if l.Verbose {
+		now := time.Now()
+		formattedTime := now.Format(time.RFC3339)
+		color.Green("%s [INFO] %s", formattedTime, message)
+	} else {
+		fmt.Printf("%s\n", message)
+	}
+}
+
+func (l *Logger) Error(message string) {
+	now := time.Now()
+	formattedTime := now.Format(time.RFC3339)
+	color.Red("%s [ERROR] %s", formattedTime, message)
+}
+
+func (l *Logger) Warning(message string) {
+	now := time.Now()
+	formattedTime := now.Format(time.RFC3339)
+	color.Yellow("%s [WARN] %s", formattedTime, message)
+}
+
+func (l *Logger) Debug(message string) {
+	now := time.Now()
+	formattedTime := now.Format(time.RFC3339)
+	if l.Verbose {
+		fmt.Printf("%s [DEBUG] %s\n", formattedTime, message)
+	}
+}
