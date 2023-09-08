101b0dc62c6318f6416b0193268058ad0fbe1aeb
Celso Inacio <celso@enssure.com.br>
2023-09-08 18:45:39 -0300

    add: config handler

---
+++ cmd/config.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/ceelsoin/subzero/domain"
+	"github.com/ceelsoin/subzero/infra"
+	"github.com/ceelsoin/subzero/utils"
+	"github.com/spf13/cobra"
+)
+
+var checkboxList []string
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Configure your experience using subzero cli",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		prompt := &survey.MultiSelect{
+			Message: "Select the subtitle providers that you wanna enable:",
+			Options: []string{"OpenSubtitles.com"},
+		}
+		err := survey.AskOne(prompt, &checkboxList)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if utils.Contains(checkboxList, "OpenSubtitles.com") {
+			var apiKey string
+			promptAPIKey := &survey.Input{
+				Message: "Please paste your OpenSubtitles.org API key:",
+			}
+			err := survey.AskOne(promptAPIKey, &apiKey, survey.WithValidator(survey.Required))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Agora você pode usar a apiKey como necessário
+			fmt.Println("API Key:", apiKey)
+
+			c := infra.NewConfigurationInstance()
+
+			cfg := domain.Configuration{}
+			cfg.EnableOpenSubtitles = true
+			cfg.OpenSubtitlesApiKey = apiKey
+
+			err = c.SaveConfig(cfg)
+			if err != nil {
+				logger.Error(fmt.Sprintf("Error while saving configuration: %s", err.Error()))
+			}
+		}
+
+		logger.Info("Configuration finished, welcome to Subzero CLI, you can reconfigure running `subzero config`")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
--- go.mod
+++ go.mod
@@ -3,18 +3,27 @@ module github.com/ceelsoin/subzero
 go 1.18
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.7 // indirect
 	github.com/TheForgotten69/go-opensubtitles v1.0.0 // indirect
 	github.com/briandowns/spinner v1.23.0 // indirect
 	github.com/cyruzin/golang-tmdb v1.5.1 // indirect
 	github.com/fatih/color v1.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/schollz/progressbar/v3 v3.13.1 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/term v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
